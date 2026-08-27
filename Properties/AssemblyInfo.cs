143817620ace4cf45a627ea92fd81b114547d401
Mykola <42931914+Mykola-Om@users.noreply.github.com>
2026-08-27 13:36:43 +0300

    docs: add MIT licence
    The repository is public but had no licence at all, which technically left
    nobody with the right to use it. Also refreshed the assembly copyright, which
    still read "Copyright ©  2024" with an empty company field.

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("KeyboardLayoutSwitcher")]
-[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyCopyright("Copyright © Mykola 2026")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
